d09b5d15594f81e5a68403dc8f19e583c5d9dbaf
Vitor Sousa Mesquita <72332412+VitorsmX@users.noreply.github.com>
2024-09-02 20:25:56 -0300

    fix: img url prelink load

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -12,8 +12,12 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
     <>
       <Head>
         <title>{title}</title>
-        <meta content="https://i.imgur.com/P5AUw2b.jpeg" property="og:image" />
-        <meta content={ogImageUrl} property="og:image" />
+        <meta content="https://i.imgur.com/P5AUw2b.jpeg" property="og:image:url" />
+        <meta content={ogImageUrl} property="og:image:url" />
+        <meta content='jpeg' property='og:image:type'></meta>
+        <meta content="400" property="og:image:width" />
+        <meta content="300" property="og:image:height" />
+        <meta property="og:image:alt" content="Portfolio e divulgação para você criar websites profissionais" />
         <meta content={description} name="description" />
 
         {/* several domains list the same content, make sure google knows we mean this one. */}
@@ -28,9 +32,6 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
         <meta content={`${twitterUrl}${pathname}`} property="og:url" />
-        <meta content="800" property="og:image:width" />
-        <meta content="400" property="og:image:height" />
-        <meta property="og:image:alt" content="Portfolio e divulgação para você criar websites profissionais" />
 
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
         <meta content={title} name="twitter:title" />
